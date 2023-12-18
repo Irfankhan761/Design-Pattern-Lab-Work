c24bf81e04264281ff020317301fd649dbc8b6fc
Irfankhan761 <irfandk1994@gmail.com>
2023-12-18 20:11:35 +0500

    added class diagram

--- LAB13/src/Memento_Pattern_Task/Rectangle.java
+++ LAB13/src/Memento_Pattern_Task/Rectangle.java
@@ -11,7 +11,6 @@ public class Rectangle extends BaseShape {
 
     private int width;
     private int height;
-
     public Rectangle(int x, int y, int width, int height, Color color) {
         super(x, y, color);
         this.width = width;
